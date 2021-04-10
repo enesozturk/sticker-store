aa6229d8be89b4ebe921103b059ae80f72b31aa7
Enes Ozturk <enesozturk.d@gmail.com>
2021-04-10 23:07:30 +0300

    chore: update tailwind configs

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,23 @@
 module.exports = {
-  purge: [],
+  mode: "jit",
+  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    screens: {
+      xs: "414px",
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      "2xl": "1536px",
+    },
   },
   variants: {
-    extend: {},
+    extend: {
+      minHeight: {
+        48: "12rem",
+      },
+    },
   },
   plugins: [],
 };
