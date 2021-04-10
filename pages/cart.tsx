77314e34b2e6817973aff0c911c4eb47657b693b
Enes Ozturk <enesozturk.d@gmail.com>
2021-04-10 23:18:05 +0300

    chore: add cart page

---
+++ pages/cart.tsx
@@ -0,0 +1,23 @@
+import { PageHeader } from "../components/Header";
+import { ProductCartItem } from "../components/Product";
+import { CheckoutSummary } from "../components/Checkout";
+
+function Cart({}) {
+  return (
+    <div className="flex flex-col w-full">
+      <PageHeader rightTitle="My Cart" rightSubtitle="2 products" />
+      <div className="grid grid-cols-3 w-full mt-8 gap-2">
+        <div className="col-start-1 col-end-4 sm:col-end-3">
+          <ProductCartItem />
+          <ProductCartItem />
+          <ProductCartItem />
+        </div>
+        <div className="col-start-1 col-end-4 sm:col-start-3">
+          <CheckoutSummary />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Cart;
