714a82a2e69acd91290566ab321cf7fca92e94fe
Enes Ozturk <enesozturk.d@gmail.com>
2021-04-10 23:06:59 +0300

    chore: update app layout

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,16 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+
+import { Header } from "../components/Header";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Header />
+      <div className="mx-auto max-w-4xl flex justify-between items-center px-2 pt-8">
+        <Component {...pageProps} />
+      </div>
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
