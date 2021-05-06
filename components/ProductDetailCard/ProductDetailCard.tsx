73796d2f59e9e60cc7f34657a1d2843d3333940a
Enes Ozturk <enesozturk.d@gmail.com>
2021-05-06 03:46:24 +0300

    chore: update product card

--- components/ProductDetailCard/ProductDetailCard.tsx
+++ components/ProductDetailCard/ProductDetailCard.tsx
@@ -1,20 +1,32 @@
+import Image from "next/image";
 import HeartIcon from "../Icon/HeartIcon";
 import { Button, IconButton, TypeButton } from "../Button";
 
-const ProductCard = () => {
+const ProductCard = ({ item }) => {
+  const image = item.image?.[0].url;
+  const imageLoader = ({ src }) => {
+    return `http://localhost:1337${src}`;
+  };
+
   return (
     <div className="rounded-2xl bg-white flex flex-col sm:flex-row w-full shadow overflow-hidden w-full  mb-4">
       <div className="flex-1 p-8 flex justify-center items-center">
-        <img className="h-48" src="/images/docker.png" />
+        <Image
+          loader={imageLoader}
+          src={image}
+          alt="product image"
+          width={200}
+          height={200}
+          className="h-48"
+        />
       </div>
       <div className="flex-1 flex flex-col items-start bg-gray-100 p-8">
-        <span className="text-2xl font-semibold mb-4">Docker Sticker</span>
-        <span className="text-gray-400 mb-4">
-          Docker helps developers bring their ideas to life by conquering the
-          complexity of app development.
-        </span>
+        <span className="text-2xl font-semibold mb-4">{item.title}</span>
+        <span className="text-gray-400 mb-4">{item.description}</span>
         <div className="px-4 py-2 bg-gray-200 rounded-lg mb-4">
-          <span className="text-2xl text-gray-600 font-semibold">10₺</span>
+          <span className="text-2xl text-gray-600 font-semibold">
+            ₺{item.price}
+          </span>
         </div>
         <span className="font-semibold text-gray-400 mb-1">Type</span>
         <div className="flex gap-1 mb-4">
