495192dc94b00747dc15fe6d445a0f3c246d6642
Enes Ozturk <enesozturk.d@gmail.com>
2021-05-06 03:47:14 +0300

    chore: get product details and update product detail card comp

--- components/Product/ProductCard.tsx
+++ components/Product/ProductCard.tsx
@@ -1,13 +1,26 @@
 import Link from "next/link";
+import Image from "next/image";
+
+type ProductProps = {
+  id: number;
+  title: string;
+  price: string;
+};
 
 type ProductCardProps = {
+  item: ProductProps;
   isFirst?: boolean;
   isLast?: boolean;
 };
 
-const ProductCard = ({ isFirst, isLast }: ProductCardProps) => {
+const ProductCard = ({ item, isFirst, isLast }: ProductCardProps) => {
+  const image = item.image?.[0].url;
+  const imageLoader = ({ src }) => {
+    return `http://localhost:1337${src}`;
+  };
+
   return (
-    <Link href="/product/1">
+    <Link href={`/product/${item.id}`}>
       <a
         className={`flex flex-col rounded-2xl bg-white overflow-hidden shadow mb-4 w-full hover:bg-gray-100 hover:cursor-pointer ${
           isFirst
@@ -18,11 +31,17 @@ const ProductCard = ({ isFirst, isLast }: ProductCardProps) => {
         }`}
       >
         <div className="flex justify-center items-center">
-          <img src="/images/docker.png" />
+          <Image
+            loader={imageLoader}
+            src={image}
+            alt="product image"
+            width={200}
+            height={200}
+          />
         </div>
         <div className="flex flex-col bg-gray-100 p-4">
-          <span className="font-semibold">Docker Sticker</span>
-          <span className="font-medium text-gray-400">10₺</span>
+          <span className="font-semibold">{item.title}</span>
+          <span className="font-medium text-gray-400">₺{item.price}</span>
         </div>
       </a>
     </Link>
--- pages/product/[id].tsx
+++ pages/product/[id].tsx
@@ -1,7 +1,23 @@
 import ProductDetailCard from "../../components/ProductDetailCard";
+import { getProduct, getProducts } from "../../utils/api";
 
-function Sticker({}) {
-  return <ProductDetailCard />;
+function Sticker({ ...props }) {
+  const { product } = props;
+  return <ProductDetailCard item={product} />;
+}
+
+export async function getStaticPaths() {
+  const products = await getProducts();
+  const paths = products.map((product) => {
+    return { params: { id: product.id.toString() } };
+  });
+  return { paths, fallback: "blocking" };
+}
+
+export async function getStaticProps({ params }) {
+  const product = await getProduct(params.id);
+
+  return { props: { product }, revalidate: 1000 };
 }
 
 export default Sticker;
