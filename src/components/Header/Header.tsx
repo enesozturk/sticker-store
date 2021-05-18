64f92fd58736a730407a490effc512abbc2ec262
Enes Ozturk <enesozturk.d@gmail.com>
2021-05-19 01:01:48 +0300

    chore: add icon to header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,15 +2,21 @@ import Link from "next/link";
 
 import { IconButton } from "../Button";
 import Search from "../Icon/Search";
+import Image from "../Image";
 import CartButton from "./CartButton";
 
 const Header = ({}) => {
   return (
     <div className="w-screen shadow p-2">
       <div className="container mx-auto max-w-4xl flex justify-between items-center px-2">
-        <Link href="/">
-          <a className="text-xl font-semibold">Sticker Store</a>
-        </Link>
+        <div className="flex justify-between items-center">
+          <Link href="/">
+            <a className="text-xl font-semibold flex justify-between items-center">
+              <Image src="/sticker-store-128.png" size={32} />
+              <span className="ml-4">Sticker Store</span>
+            </a>
+          </Link>
+        </div>
         <div className="flex items-center gap-x-2">
           <IconButton icon={<Search />} href="#" />
           <CartButton />
