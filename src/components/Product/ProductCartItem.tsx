77ce9a2877798b7342b1913562d73b10d060ca15
Enes Ozturk <enesozturk.d@gmail.com>
2021-05-19 00:19:52 +0300

    chore: update product cart comps

--- src/components/Product/ProductCartItem.tsx
+++ src/components/Product/ProductCartItem.tsx
@@ -10,7 +10,6 @@ type ProductCartItemProps = {
 
 const ProductCartItem = ({ item }: ProductCartItemProps) => {
   const { removeProductFromCart, addProductToCart } = useAppContext();
-  const image = item.image[0]?.url;
 
   const handleAddProductToCart = () => {
     addProductToCart(item);
@@ -24,7 +23,7 @@ const ProductCartItem = ({ item }: ProductCartItemProps) => {
     <div className="rounded-2xl bg-white flex flex-col xs:flex-row w-full shadow overflow-hidden w-full mb-4">
       <div className="flex flex-row xs:flex-col sm:flex-row flex-1">
         <div className="w-40 flex justify-center items-center">
-          <Image src={image} size={150} />
+          <Image src={item.image} size={150} />
         </div>
         <div className="flex-1 flex flex-col items-start p-4">
           <span className="text-xl font-semibold mb-2">{item.title}</span>
