8ce104848cc94166ff953b3c1c4c93cf5b0017d6
Enes Ozturk <enesozturk.d@gmail.com>
2021-07-13 22:35:24 +0300

    chore: update order completed page layout and styles

--- pages/order-completed.tsx
+++ pages/order-completed.tsx
@@ -8,9 +8,17 @@ function OrderCompleted() {
 
   return (
     <Page title="Order Completed">
-      <div className="flex flex-col w-full">
-        <h1>Your order completed 🔥</h1>
+      <div className="flex flex-col items-center w-full bg-gray-100 p-8 rounded-xl">
+        <h1 className="text-8xl font-semibold	text-center pt-8">🎉</h1>
+        <h1 className="text-4xl font-semibold	text-center pt-8">
+          Your order completed.
+        </h1>
+        <p className="text-lg text-center pt-4">
+          We will send you the details via email and SMS to keep you posted
+          about your order!
+        </p>
         <Button
+          className="bg-gray-200 text-gray-500 w-auto justify-center mt-8 hover:bg-gray-300 transition"
           onClick={() => {
             router.push("/");
           }}
