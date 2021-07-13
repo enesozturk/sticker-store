ad71c65d07e22e4e2b3c9f2759046b37ff6812f0
Enes Ozturk <enesozturk.d@gmail.com>
2021-07-13 03:14:49 +0300

    feat: add craftgate integration with new api route

---
+++ pages/api/payment.ts
@@ -0,0 +1,43 @@
+const Craftgate = require("@craftgate/craftgate");
+
+const craftgate = new Craftgate.Client({
+  apiKey: process.env.NEXT_PUBLIC_CRAFTGATE_API_KEY,
+  secretKey: process.env.NEXT_PUBLIC_CRAFTGATE_SECRET_KEY,
+  baseUrl: "https://sandbox-api.craftgate.io",
+});
+
+const paymentDetails = {
+  walletPrice: 0.0,
+  installment: 1,
+  conversationId: "456d1297-908e-4bd6-a13b-4be31a6e47d5",
+  currency: Craftgate.Model.Currency.TRY,
+  paymentGroup: Craftgate.Model.PaymentGroup.ListingOrSubscription,
+  card: {
+    cardHolderName: "Haluk Demir",
+    cardNumber: "5258640000000001",
+    expireYear: "2044",
+    expireMonth: "07",
+    cvc: "000",
+  },
+};
+
+export default (req, res) => {
+  const { items } = JSON.parse(req.body);
+
+  const totalPrice = items.reduce((acc, item) => acc + item.price, 0);
+
+  craftgate
+    .payment()
+    .createPayment({
+      ...paymentDetails,
+      price: totalPrice,
+      paidPrice: totalPrice,
+      items,
+    })
+    .then(function (result) {
+      res.status(200).json({ message: "Payment successful", ...result });
+    })
+    .catch(function (err) {
+      throw Error(err);
+    });
+};
