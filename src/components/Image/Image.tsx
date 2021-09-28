5907e573bc8ecebcc506ad851792404ed6bdd089
Enes Ozturk <enesozturk.d@gmail.com>
2021-09-28 21:59:55 +0300

    refactor: update image component

--- src/components/Image/Image.tsx
+++ src/components/Image/Image.tsx
@@ -8,17 +8,8 @@ type ImageProps = {
 };
 
 const Image = ({ local, src, size, className, ...props }: ImageProps) => {
-  const imageLoader = ({ src }) => {
-    return process.env.NODE_ENV == "development"
-      ? local
-        ? src
-        : `http://localhost:1337${src}`
-      : `${process.env.NEXT_PUBLIC_STRAPI_API_URL}${src}`;
-  };
-
   return (
     <NextImage
-      loader={imageLoader}
       src={src}
       alt="product image"
       width={size}
