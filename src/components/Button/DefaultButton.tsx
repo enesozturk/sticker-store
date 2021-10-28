5f5a857d65f7bc0d419167e3856f839bebd1aeae
Enes Ozturk <enesozturk.d@gmail.com>
2021-10-28 10:35:16 +0300

    feat: add loading state to button

--- src/components/Button/DefaultButton.tsx
+++ src/components/Button/DefaultButton.tsx
@@ -1,9 +1,17 @@
-const Button = ({ className = "", text, ...props }) => {
+import { Spinner } from "../Icon";
+
+const Button = ({ className = "", text, loading = false, ...props }) => {
   return (
     <button
       {...props}
-      className={`flex flex-row items-center py-2 px-4 transition bg-blue-100 hover:bg-blue-200 text-blue-400 rounded-xl font-medium focus:outline-none focus:ring focus:border-blue-100 h-12 ${className}`}
+      onClick={loading ? null : props.onClick}
+      className={`flex flex-row items-center py-2 px-4 transition bg-blue-100 focus:outline-none ${
+        loading
+          ? "cursor-default text-blue-300"
+          : "text-blue-400 focus:ring focus:border-blue-100 cursor-pointer hover:bg-blue-200"
+      } rounded-xl font-medium h-12 ${className}`}
     >
+      {loading ? <Spinner /> : null}
       {text || "Button"}
     </button>
   );
--- src/components/Icon/index.ts
+++ src/components/Icon/index.ts
@@ -4,3 +4,4 @@ export { default as ShoppingCartIcon } from "./ShoppingCart";
 export { default as TrashIcon } from "./TrashIcon";
 export { default as MinusIcon } from "./MinusIcon";
 export { default as PlusIcon } from "./PlusIcon";
+export { default as Spinner } from "./Spinner";
