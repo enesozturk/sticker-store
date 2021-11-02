4734d111fa3f667ddcc733fcd0d7afcda62f995e
Enes Ozturk <enesozturk.d@gmail.com>
2021-11-02 23:16:49 +0300

    refactor: add section to home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import Page from "../src/components/Page";
 
 import { ProductCard } from "../src/components/Product";
+import Section from "../src/components/Section";
 import { Product } from "../src/types/product";
 import { getRecords } from "../src/utils/airtable";
 
@@ -9,10 +10,16 @@ export default function Home({ ...props }) {
 
   return (
     <Page title="Sticker Store">
-      <div className="grid grid-cols-1 xs:grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-x-1 sm:gap-x-4 w-full">
-        {products?.map((item: Product, index: number) => {
-          return <ProductCard key={index} item={item} />;
-        })}
+      <div className="flex flex-col w-full">
+        <Section
+          title="Sticker Store"
+          description="Find the best stickers and make you environment fun"
+        />
+        <div className="grid grid-cols-1 xs:grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-x-1 sm:gap-x-4 w-full mb-8">
+          {products?.map((item: Product, index: number) => {
+            return <ProductCard key={index} item={item} />;
+          })}
+        </div>
       </div>
     </Page>
   );
