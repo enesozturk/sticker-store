6a1c18d85d72427260a4cf1ee86793e477b0e971
Enes Ozturk <enesozturk.d@gmail.com>
2021-11-02 12:07:40 +0300

    refactor: update order create method with form data

--- src/hooks/useCheckout.ts
+++ src/hooks/useCheckout.ts
@@ -16,15 +16,30 @@ export const useCheckout = () => {
 
   useEffect(() => {}, []);
 
-  const handleCreatePayment = () => {
+  const generateAddressString = (_formData) => {
+    const { firstName, lastName, address, city, country, state, postalCode } =
+      _formData;
+    return `${firstName} ${lastName}, ${address}, Postal Code: ${postalCode}, State: ${state},  ${city} / ${country}`;
+  };
+
+  const handleCreatePayment = (formData) => {
     createPayment(getCartItemsForOrder()).then((response) => {
       if (response.ok) {
-        createRecord("Order", [{ fields: { total_price: productTotal } }]).then(
-          () => {
-            resetCart();
-            router.replace("/order-completed");
-          }
-        );
+        createRecord("Order", [
+          {
+            fields: {
+              total_price: productTotal,
+              first_name: formData.firstName,
+              last_name: formData.lastName,
+              email: formData.email,
+              phone: formData.phone,
+              address: generateAddressString(formData),
+            },
+          },
+        ]).then(() => {
+          resetCart();
+          router.replace("/order-completed");
+        });
       }
     });
   };
