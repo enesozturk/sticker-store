75a9d0ccd141df6397fbc4b115d26783da27eb5c
Enes Ozturk <enesozturk.d@gmail.com>
2021-11-02 10:42:28 +0300

    refactor: forward ref for input

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,20 +1,31 @@
+import { forwardRef } from "react";
+
 type InputProps = {
   title?: string;
+  error?: {
+    type: string;
+    message: string;
+  };
 } & React.InputHTMLAttributes<HTMLInputElement>;
 
-const Input = ({ title, ...props }: InputProps) => {
-  return (
-    <div className="flex flex-col justify-start items-start flex-1">
-      <span className="text-gray-600 font-medium text-sm mb-1">{title}</span>
-      <input
-        {...props}
-        onClick={(e) => {
-          e.stopPropagation();
-        }}
-        className="px-4 py-1 border border-gray-300 rounded-md focus:outline-none h-10 w-full shadow"
-      />
-    </div>
-  );
-};
+type InputRef = React.ForwardRefExoticComponent<HTMLInputElement>;
+
+const Input = forwardRef(
+  ({ title, error, ...props }: InputProps, ref: InputRef) => {
+    return (
+      <div className="flex flex-col justify-start items-start flex-1">
+        <span className="text-gray-600 font-medium text-sm mb-1">{title}</span>
+        <input
+          ref={ref}
+          {...props}
+          onClick={(e) => {
+            e.stopPropagation();
+          }}
+          className="px-4 py-1 border border-gray-300 rounded-md focus:outline-none h-10 w-full shadow"
+        />
+      </div>
+    );
+  }
+);
 
 export default Input;
