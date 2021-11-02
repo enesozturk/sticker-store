b82152cccf6cb6a67752d760c33ed09112366dd3
Enes Ozturk <enesozturk.d@gmail.com>
2021-11-03 00:23:37 +0300

    refactor: update footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -9,17 +9,21 @@ import { PlusIcon } from "../Icon";
 function Footer() {
   return (
     <footer className="border-t">
-      <div className="mx-auto max-w-4xl flex justify-between items-start px-2 pt-8 flex-1 mb-8">
-        <div className="flex flex-col items-start justify-between">
+      <div className="mx-auto max-w-4xl flex flex-col sm:flex-row justify-between items-start px-2 pt-8 flex-1 mb-8">
+        <div className="flex flex-col items-start justify-between mb-4">
           <div className="flex items-center justify-start mb-8">
             <Image local src="/sticker-store-128.png" size={32} />
           </div>
           <span className="text-gray-400">
-            © 2021 EO, Inc. All rights reserved.
+            © 2021{" "}
+            <a href="https://github.com/enesozturk" target="_blank">
+              Enes Ozturk
+            </a>
+            . All rights reserved.
           </span>
         </div>
-        <div className="flex-1 flex justify-end">
-          <div className="flex flex-col items-end">
+        <div className="flex-1 flex justify-end mb-4">
+          <div className="flex flex-col items-start sm:items-end">
             <Link href={"/"}>
               <a className="mb-2 text-gray-500 hover:text-gray-600">Home</a>
             </Link>
@@ -31,7 +35,7 @@ function Footer() {
             </Link>
           </div>
         </div>
-        <div className="flex-1 flex justify-end">
+        <div className="flex-1 flex justify-end mb-4">
           <div className="flex flex-col items-start">
             <Link href={"/terms-of-use"}>
               <a className="mb-2 text-gray-500 hover:text-gray-600">
@@ -45,7 +49,7 @@ function Footer() {
             </Link>
           </div>
         </div>
-        <div className="flex-1 flex flex-col items-end">
+        <div className="flex-1 flex flex-col items-end mb-4">
           <IconButton onClick={() => {}} icon={<span>TR</span>} />
         </div>
       </div>
