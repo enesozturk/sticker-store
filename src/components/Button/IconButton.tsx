a34cb0dd2fc1656e778bcb4a6252b89a6e683f9f
Enes Ozturk <enesozturk.d@gmail.com>
2021-11-07 17:13:41 +0300

    refactor: add new props to icon button

--- src/components/Button/IconButton.tsx
+++ src/components/Button/IconButton.tsx
@@ -3,19 +3,21 @@ import Link from "next/link";
 type IconButtonProps = {
   href?: string;
   icon?: React.ReactNode;
+  locale?: string;
   text?: string | number | null;
   onClick?: () => void;
   round?: boolean;
-  small?: boolean;
+  shallow?: boolean;
 };
 
 const IconButton = ({
   text,
   icon,
+  locale,
   onClick,
   round,
   href,
-  small,
+  shallow,
 }: IconButtonProps) => {
   if (onClick)
     return (
@@ -33,7 +35,7 @@ const IconButton = ({
     );
 
   return (
-    <Link href={href || "#"}>
+    <Link href={href || "#"} locale={locale} shallow={shallow}>
       <a
         className={`shadow rounded-2xl focus:outline-none focus:ring focus:border-blue-100 flex justify-center p-3 hover:bg-gray-100 bg-white ${
           round && "rounded-full"
