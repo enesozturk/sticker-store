03bb20aae8abf8be8ee0730b6e6443071ba239ea
Enes Ozturk <enesozturk.d@gmail.com>
2021-11-11 10:41:12 +0300

    refactor: add class name prop to heart icon

--- src/components/Icon/HeartIcon.tsx
+++ src/components/Icon/HeartIcon.tsx
@@ -1,8 +1,8 @@
-const HeartIcon = () => {
+const HeartIcon = ({ className }) => {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      className="h-6 w-6"
+      className={`h-6 w-6 ${className || ""}`}
       fill="none"
       viewBox="0 0 24 24"
       stroke="currentColor"
