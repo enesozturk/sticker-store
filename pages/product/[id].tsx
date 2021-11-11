2f0296648c1ce79ac11fe276797a4793d3d3a36b
Enes Ozturk <enesozturk.d@gmail.com>
2021-11-11 10:43:45 +0300

    refactor: update product detail page props as server side fetch

--- pages/product/[id].tsx
+++ pages/product/[id].tsx
@@ -1,8 +1,9 @@
+import type { GetServerSideProps } from "next";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 
 import Page from "../../src/components/Page";
 import ProductDetailCard from "../../src/components/ProductDetailCard";
-import { getRecord, getRecords } from "../../src/utils/airtable";
+import { getRecord } from "../../src/utils/airtable";
 
 function Sticker({ ...props }) {
   const { product } = props;
@@ -14,15 +15,9 @@ function Sticker({ ...props }) {
   );
 }
 
-export async function getStaticPaths() {
-  const products = await getRecords("Product");
-  const paths = products?.map((product) => {
-    return { params: { id: product.id.toString() } };
-  });
-  return { paths, fallback: "blocking" };
-}
-
-export async function getStaticProps({ params, locale }) {
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const { params, locale } = context;
+  // @ts-ignore
   const product = await getRecord("Product", params.id);
 
   return {
@@ -30,8 +25,7 @@ export async function getStaticProps({ params, locale }) {
       product,
       ...(await serverSideTranslations(locale, ["common"])),
     },
-    revalidate: 3600,
   };
-}
+};
 
 export default Sticker;
