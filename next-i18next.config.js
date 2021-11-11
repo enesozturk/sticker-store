b49af4808e4b316494fc0528dd4fe06668559a3d
Enes Ozturk <enesozturk.d@gmail.com>
2021-11-11 12:47:37 +0300

    chore: add locale path to i18next configs

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,7 +1,10 @@
+const path = require("path");
+
 module.exports = {
   i18n: {
     locales: ["en-US", "tr-TR"],
     defaultLocale: "en-US",
     localeDetection: false,
   },
+  localePath: path.resolve("./public/locales"),
 };
