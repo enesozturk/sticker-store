29f243ca64fdaa99e5b68ecb3185fdc9b7f543d6
Enes Ozturk <enesozturk.d@gmail.com>
2021-11-11 10:44:03 +0300

    refactor: use likes hook and update styles

--- src/components/ProductDetailCard/ProductDetailCard.tsx
+++ src/components/ProductDetailCard/ProductDetailCard.tsx
@@ -4,10 +4,12 @@ import HeartIcon from "../Icon/HeartIcon";
 import { Button, IconButton, TypeButton } from "../Button";
 import { useShoppingCardContext } from "../../hooks/useShoppingCardContext";
 import Image from "../Image";
+import { useLikes } from "../../hooks";
 
-const ProductCard = ({ item }) => {
+const ProductCard = ({ item, likeCount = 0 }) => {
   const { t: translate } = useTranslation();
   const { addProductToCart } = useShoppingCardContext();
+  const { liked, handleLikeProduct } = useLikes({ productId: item.id });
 
   const handleAddProductToCart = () => {
     addProductToCart(item);
@@ -21,13 +23,13 @@ const ProductCard = ({ item }) => {
       <div className="flex-1 flex flex-col items-start bg-gray-100 p-8">
         <span className="text-2xl font-semibold mb-4">{item.title}</span>
         <span className="text-gray-400 mb-4">{item.description}</span>
-        <div className="px-4 py-2 bg-gray-200 rounded-lg mb-4">
+        <div className="px-4 py-2 bg-gray-200 rounded-lg mb-8">
           <span className="text-2xl text-gray-600 font-semibold">
             ₺{item.price}
           </span>
         </div>
         <span className="font-medium text-gray-400 mb-1">Type</span>
-        <div className="flex gap-1 mb-4">
+        <div className="flex gap-1 mb-8">
           <TypeButton text="Normal" description="Standart material" />
           <TypeButton text="Bold" description="Double protection" isActive />
         </div>
@@ -36,7 +38,16 @@ const ProductCard = ({ item }) => {
             onClick={handleAddProductToCart}
             text={translate("product.details.add_to_cart")}
           />
-          <IconButton icon={<HeartIcon />} />
+          <IconButton
+            icon={
+              <HeartIcon
+                className={`${liked ? "fill-red" : "fill-current"} ${
+                  liked ? "stroke-red" : "stroke-gray"
+                }`}
+              />
+            }
+            onClick={handleLikeProduct}
+          />
         </div>
       </div>
     </div>
